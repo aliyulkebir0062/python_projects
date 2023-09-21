03629e2bf389c8220d8052aa45b6e77da35b6bf5
Ali Yülkebir Güneş <104678795+aliyulkebir0062@users.noreply.github.com>
2023-09-21 23:01:10 +0300

    QR_kod.py
    Bu kod, bir bilgisayar kamerası kullanarak barkod ve QR kodlarını tanır, verilerini ekrana basar ve tanınan kodları yeşil bir çokgenle işaretler.

---
+++ QR_kod.py
@@ -0,0 +1,29 @@
+import cv2
+from pyzbar.pyzbar import decode
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    barcodes = decode(frame)
+
+    for barcode in barcodes:
+        barcode_data = barcode.data.decode('utf-8')
+        barcode_type = barcode.type
+        #Tanınan barkod veya QR kod bilgisini ekrana yazdırır
+        print(f'Tür: {barcode_type}, Veri: {barcode_data}')
+        
+        # Görüntü üzerinde kodları işaretle
+        points = np.array(barcode.polygon, dtype=np.int32)
+        if len(points) >= 4:
+            cv2.polylines(frame, [points], isClosed=True, color=(0, 255, 0), thickness=2)
+
+    cv2.imshow('Barkod ve QR Kod Tanıma', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
